86b3c3c71835e8edd34da31c86cc4cac19f35bc8
KimJaeHyeok <master_catch@naver.com>
2023-06-26 09:13:45 +0900

    23.06.26(JH)-FirstCommit

---
+++ schemas/index.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
+});
